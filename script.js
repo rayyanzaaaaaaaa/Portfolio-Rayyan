7fd3a7f214f9c47259a9d5128c0207974938a4f6
rayyanzaaaaaaaa <167841569+rayyanzaaaaaaaa@users.noreply.github.com>
2025-06-24 13:55:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+function hamburg(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
+}
+function cancel(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}
+// Typewriter Effect
+const texts = [
+    " FRONT-END DEVELOPER",
+    " SISWA SMKN 69 JAKARTA",
+]
+let speed  =100;
+const textElements = document.querySelector(".typewriter-text");
+let textIndex = 0;
+let charcterIndex = 0;
+function typeWriter(){
+    if (charcterIndex < texts[textIndex].length){
+        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
+        charcterIndex++;
+        setTimeout(typeWriter, speed);
+    }
+    else{
+        setTimeout(eraseText, 1000)
+    }
+}
+function eraseText(){
+    if(textElements.innerHTML.length > 0){
+        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
+        setTimeout(eraseText, 50)
+    }
+    else{
+        textIndex = (textIndex + 1) % texts.length;
+        charcterIndex = 0;
+        setTimeout(typeWriter, 500)
+    }
+}
+window.onload = typeWriter
+const hamburgerIcon = document.getElementById('hamburger-icon');
+const cancelIcon = document.getElementById('cancel-icon');
+const dropdownMenu = document.getElementById('dropdown-menu');
+const dropdownLinks = dropdownMenu.querySelectorAll('.links a');
+if (hamburgerIcon) {
+    hamburgerIcon.addEventListener('click', toggleDropdown);
+}
+if (cancelIcon) {
+    cancelIcon.addEventListener('click', toggleDropdown);
+}
